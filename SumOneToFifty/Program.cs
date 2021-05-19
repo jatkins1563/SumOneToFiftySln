61509e76eed59e62e7871c984153cc5f1489ed8f
John Atkins <jdatkins@tql.com>
2021-05-19 16:36:26 -0400

    Add project files.

---
+++ SumOneToFifty/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SumOneToFifty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sum = 0;
+
+            for (var i = 1; i < 51; i++)
+            {
+                if (i % 5 == 0 || i % 7 == 0)
+                {
+                    sum += i;
+                }
+            }
+
+            Console.WriteLine($"Sum is {sum}.");
+        }
+    }
+}
